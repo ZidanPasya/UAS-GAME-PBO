e59a8699a34c27ee3c6cda9fc8b59c264d258f3e
Reza Nur Ramadhan <92014396+RzaNrRmdhn@users.noreply.github.com>
2022-12-08 16:34:28 +0700

    Update Game.js

--- Game.js
+++ Game.js
@@ -30,3 +30,55 @@ class Entity{
         }
     }
 }
+
+class Monster extends Entity{
+    constructor(life, color, effect, type){
+        this.life = life;
+        this.color = color;
+        this.effect = effect;
+        this.type = type;
+    }
+
+    moveRandom(){
+
+    }
+
+    saveScore(){
+
+    }
+}
+
+class Hero extends Entity{
+    constructor(life, score, skill){
+        this.life = life;
+        this.score = score;
+        this.skill = skill;
+    }
+
+    increaseScore(){
+
+    }
+
+    calculateLife(){
+
+    }
+
+    saveScore(){
+
+    }
+}
+
+class Map{
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+
+    init(){
+
+    }
+
+    move(){
+        
+    }
+}
